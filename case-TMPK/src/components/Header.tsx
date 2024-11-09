e8d99119231b45a59fb88c7c8b8830d0252f85c8
Skelesheit <Skelesheit@gmail.com>
2024-11-09 20:52:38 +0300

    первично исправлены некоторые элементы у которых всё шло по одному месту, немного подердачить и вполне пойдёт.

--- case-TMPK/src/components/Header.tsx
+++ case-TMPK/src/components/Header.tsx
@@ -15,15 +15,15 @@ export default function Header({
   onOptionsClick,
 }: ButtonProps) {
   return (
-    <main className="flex h-16 w-full items-center justify-between gap-2 bg-teal-600 p-5 dark:bg-gray-800">
+    <main className="flex h-16 w-full items-center justify-between gap-2 bg-tmpk-blue">
       <div className="flex h-12 flex-wrap items-center gap-1">
-        <Avatar img="src\assets\logo.svg" alt="logo" rounded />
-        <h1 className="text-2xl dark:text-white ">ТМПК</h1>
+        <Avatar className={'w-12'} img="src\assets\logo.svg" alt="logo" rounded />
+        <h1 className="text-2xl text-white ">ТМПК</h1>
       </div>
 
       <div className="navBtn flex h-12 w-3/12 gap-2 p-0">
         <Button
-          className="bg-cyan-500 p-1 dark:bg-teal-900"
+          className="bg-tmpk-blue p-1"
           onClick={onNavigationClick}
         >
           Navigation
--- case-TMPK/src/components/NavComponents/Options.tsx
+++ case-TMPK/src/components/NavComponents/Options.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { DarkThemeToggle } from "flowbite-react";
+import {Checkbox, DarkThemeToggle, Label, TextInput} from "flowbite-react";
 import { Button } from "flowbite-react";
 
 interface OptionsProps {
@@ -8,17 +8,33 @@ interface OptionsProps {
 
 export default function Options({ onClose }: OptionsProps) {
   return (
-    <main className="fixed flex size-max items-center justify-center bg-gray-800">
-      <div className="flex h-3/5 w-11/12 flex-col items-start justify-center gap-3 rounded-2xl bg-slate-100 dark:bg-gray-800">
-        <div className="flex flex-row items-center justify-between gap-2">
+    <main className="fixed flex size-full items-center justify-center bg-gray-600">
+      <div className="flex w-72 lg:w-96 flex-col items-center justify-center gap-3 rounded-2xl bg-slate-100 dark:bg-gray-800 m-2">
+        <div className="w-full flex flex-row items-center justify-between gap-2 p-2">
           <DarkThemeToggle className="bg-white" />
-          <h1 className="font-bold">Options</h1>
-          <Button onClick={onClose}>X</Button>
+          <h1 className="font-bold dark:text-white">Options</h1>
+          <Button className="text-black bg-white dark:text-white dark:bg-slate-800"
+                  onClick={onClose}>X</Button>
         </div>
         <div className="flex flex-col justify-center gap-2">
-          <Button>Изменение логина</Button>
-          <Button>Изменение пароля</Button>
-          <Button>Применить</Button>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="email1" value="Your login"/>
+            </div>
+            <TextInput
+                id="email1"
+                type=""
+                placeholder="login"
+                required
+            />
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="password1" value="Your password"/>
+            </div>
+            <TextInput id="password1" type="password" required/>
+          </div>
+          <Button className='my-4 bg-tmpk-blue dark:bg-tmpk-blue' type="submit">Change</Button>
         </div>
       </div>
     </main>
